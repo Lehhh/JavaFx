fae38bf0ac7258c7d3da009f002c53cbe0d561ed
lehhh <leandro.tacara@hotmail.com>
2018-08-19 17:40:59 -0300

    first class show pane with label

--- src/application/Exemplo1.java
+++ src/application/Exemplo1.java
@@ -1,5 +1,41 @@
 package application;
 
-public class Exemplo1 {
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class Exemplo1 extends javafx.application.Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		
+		//Layout
+		FlowPane noRaiz = new FlowPane();
+		
+		
+		
+		//Cena
+		Scene minhaCena = new Scene(noRaiz, 300,250);
+		
+		//Nó
+		Label label = new Label();
+		label.setText("Olá Mundo!");
+		
+		//Adicionar Elementos em cena
+		noRaiz.getChildren().add(label);
+		
+		// Adicionar a cena no palco
+		primaryStage.setScene(minhaCena);
+		
+		//Exibir o palco e e sua(s) cenas(s)
+		primaryStage.show();
+		
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
 
 }
