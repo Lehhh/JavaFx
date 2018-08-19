9799649fe9724cceaafc49465e2bffd4ed3030e7
lehhh <leandro.tacara@hotmail.com>
2018-08-19 18:11:00 -0300

    two methods of create a action on a button

--- src/application/Exemplo2.java
+++ src/application/Exemplo2.java
@@ -2,7 +2,9 @@ package application;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
@@ -17,15 +19,27 @@ public class Exemplo2 extends Application {
 		Scene minhaCena = new Scene(noRaiz,300,200);
 		Button btn = new Button();
 		btn.setText("Olá Mundo");
-		btn.setOnAction(new EventHandler<ActionEvent>() {
+		
+		//modo 1 de chamar uma action
+		btn.addEventHandler(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
 			
 			@Override
 			public void handle(ActionEvent event) {
-
+			
 				System.out.println("Olá Mundo");
-				
 			}
 		});
+		
+		//modo 2 de chamar uma action
+//		btn.setOnAction(new EventHandler<ActionEvent>() {
+//			
+//			@Override
+//			public void handle(ActionEvent event) {
+//
+//				System.out.println("Olá Mundo");
+//				
+//			}
+//		});
 		noRaiz.getChildren().add(btn);
 		primaryStage.setScene(minhaCena);
 		primaryStage.show();
